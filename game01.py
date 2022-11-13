0b30d7eb4979434b56ceba6fef811bb6e7e8413d
Kim Dongyun <edw0728@gmail.com>
2022-11-13 17:17:20 +0900

    bgm 추가

--- game01.py
+++ game01.py
@@ -3,13 +3,13 @@ import os
 import pygame as py
 import math
 
-url = os.path.dirname(os.path.abspath(__file__)) + "/image/"
+url = os.path.dirname(os.path.abspath(__file__))
 
 
 class Character(py.sprite.Sprite):
     def __init__(self):
         py.sprite.Sprite.__init__(self)
-        self.image = py.image.load(url + "sumong.png").convert_alpha()
+        self.image = py.image.load(url + "/image/sumong.png").convert_alpha()
         self.image = py.transform.scale(self.image, (40, 40))
         self.rect = self.image.get_rect()
         self.rect.x = round(SCREEN_WIDTH / 2)
@@ -22,7 +22,7 @@ class Character(py.sprite.Sprite):
 class Coin(py.sprite.Sprite):
     def __init__(self, x, y):
         py.sprite.Sprite.__init__(self)
-        self.image = py.image.load(url + "coin.png").convert_alpha()
+        self.image = py.image.load(url + "/image/coin.png").convert_alpha()
         self.image = py.transform.scale(self.image, (20, 20))
         self.rect = self.image.get_rect()
         self.rect.centerx = x
@@ -32,7 +32,7 @@ class Coin(py.sprite.Sprite):
 class Obstacle(py.sprite.Sprite):
     def __init__(self, x, y, speed, degrees=0, r=0, k=0, maxx=1280, minx=0, maxy = 720, miny = 0):
         py.sprite.Sprite.__init__(self)
-        self.image = py.image.load(url + "circle_red.png").convert_alpha()
+        self.image = py.image.load(url + "/image/circle_red.png").convert_alpha()
         self.image = py.transform.scale(self.image, (20, 20))
         self.rect = self.image.get_rect()
         self.rect.centerx = x
@@ -80,8 +80,8 @@ class Round(py.sprite.Sprite):
 
     def round01(self):
         py.sprite.Sprite.__init__(self)
-        self.image = py.image.load(url + "map01.png").convert_alpha()
-        self.color = py.image.load(url + "map01c.png").convert_alpha()
+        self.image = py.image.load(url + "/image/map01.png").convert_alpha()
+        self.color = py.image.load(url + "/image/map01c.png").convert_alpha()
         self.rect = self.image.get_rect()
         self.rect.centerx = round(SCREEN_WIDTH / 2)
         self.rect.centery = round(SCREEN_HEIGHT / 2)
@@ -125,8 +125,8 @@ class Round(py.sprite.Sprite):
 
     def round02(self):
         py.sprite.Sprite.__init__(self)
-        self.image = py.image.load(url + "map02.png").convert_alpha()
-        self.color = py.image.load(url + "map02c.png").convert_alpha()
+        self.image = py.image.load(url + "/image/map02.png").convert_alpha()
+        self.color = py.image.load(url + "/image/map02c.png").convert_alpha()
         self.rect = self.image.get_rect()
         self.rect.centerx = round(SCREEN_WIDTH / 2)
         self.rect.centery = round(SCREEN_HEIGHT / 2)
@@ -165,23 +165,24 @@ class Round(py.sprite.Sprite):
 
     def round03(self):
         py.sprite.Sprite.__init__(self)
-        self.image = py.image.load(url + "map03.png").convert_alpha()
-        self.color = py.image.load(url + "map03c.png").convert_alpha()
+        self.image = py.image.load(url + "/image/map03.png").convert_alpha()
+        self.color = py.image.load(url + "/image/map03c.png").convert_alpha()
         self.rect = self.image.get_rect()
         self.rect.centerx = round(SCREEN_WIDTH / 2)
         self.rect.centery = round(SCREEN_HEIGHT / 2)
         self.obs = []
         self.obsC = []
         self.coinl = []
-        self.coin = 10
+        self.coinl.append(Coin(640, 300))
+        self.coin = 1
         self.get_coin = 0
         character.rect.x = 355
         character.rect.y = 170
 
     def round04(self):
         py.sprite.Sprite.__init__(self)
-        self.image = py.image.load(url + "map04.png").convert_alpha()
-        self.color = py.image.load(url + "map04c.png").convert_alpha()
+        self.image = py.image.load(url + "/image/map04.png").convert_alpha()
+        self.color = py.image.load(url + "/image/map04c.png").convert_alpha()
         self.rect = self.image.get_rect()
         self.rect.centerx = round(SCREEN_WIDTH / 2)
         self.rect.centery = round(SCREEN_HEIGHT / 2)
@@ -203,11 +204,13 @@ class Round(py.sprite.Sprite):
 
         for i in self.obs + self.obsC:
             if py.sprite.collide_mask(i, character):
+                die.play()
                 stage.bool = True
                 character.death += 1
 
         for i in self.coinl:
             if py.sprite.collide_mask(i, character):
+                coin_sound.play()
                 self.coinl.remove(i)
                 self.get_coin += 1
 
@@ -227,24 +230,30 @@ class Endpoint(py.sprite.Sprite):
         py.sprite.Sprite.__init__(self)
 
     def round01(self):
-        self.image = py.image.load(url + "map01end.png").convert_alpha()
+        self.image = py.image.load(url + "/image/map01end.png").convert_alpha()
         self.rect = self.image.get_rect()
 
     def round02(self):
-        self.image = py.image.load(url + "map02end.png").convert_alpha()
+        self.image = py.image.load(url + "/image/map02end.png").convert_alpha()
         self.rect = self.image.get_rect()
 
     def round03(self):
-        self.image = py.image.load(url + "map03end.png").convert_alpha()
+        self.image = py.image.load(url + "/image/map03end.png").convert_alpha()
         self.rect = self.image.get_rect()
 
     def round04(self):
-        self.image = py.image.load(url + "map04end.png").convert_alpha()
+        self.image = py.image.load(url + "/image/map04end.png").convert_alpha()
         self.rect = self.image.get_rect()
 
     def update(self):
         screen.blit(self.image, (0, 0))
 
+
+class Start(py.sprite.Sprite):
+    def __init__(self):
+        py.sprite.Sprite.__init__(self)
+        self.image = py.image.load(url + "/image/start.png").convert_alpha()
+        self.rect = self.image.get_rect()
 
 def display_death():
     death = font.render(f"Death : {character.death:,}", True, True)
@@ -264,15 +273,30 @@ py.init()
 font = py.font.SysFont("arial", 25)
 py.display.set_caption("1")
 screen = py.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-white_image = py.image.load(url+"white.png").convert_alpha()
-white_image = py.transform.scale(white_image, (250, 50))
+coin_sound = py.mixer.Sound(url + "/music/coin.mp3")
+bgm = py.mixer.Sound(url + "/music/bgm.mp3")
+clear = py.mixer.Sound(url + "/music/next.mp3")
+die = py.mixer.Sound(url + "/music/die.mp3")
+main_image = py.image.load(url + "/image/main.png").convert_alpha()
+bgm.play(-1)
 
 character = Character()
 clock = py.time.Clock()
 stage = Round()
 
 while True:
-    clock.tick(30)
+    clock.tick(60)
+    screen.blit(main_image, (0, 0))
+    screen.blit(start_image, (0, 0))
+    for event in py.event.get():
+        if event.type == py.MOUSEBUTTONUP:
+
+
+
+
+
+while True:
+    clock.tick(60)
     for event in py.event.get():
         if event.type == py.QUIT:
             sys.exit()
@@ -303,6 +327,7 @@ while True:
             stage.bool = False
 
     if py.sprite.collide_mask(character, endpoint) and stage.coin <= stage.get_coin:
+        clear.play()
         stage.step += 1
         stage.bool = True
 
